c3669a07d41d0568d9f2447d57979c5b660ae206
Ellsworth McCullough <81348353+ellsworthmmcc@users.noreply.github.com>
2026-03-31 13:32:57 -0700

    Update Data Analysis.sql

--- Data Analysis.sql
+++ Data Analysis.sql
@@ -16,38 +16,38 @@ FROM layoffs_staging_2
 WHERE percentage_laid_off = 1
 ORDER BY funds_raised_millions DESC;
 
-SELECT company, SUM(total_laid_off) AS sum
+SELECT company, SUM(total_laid_off) AS laid_off_sum
 FROM layoffs_staging_2
 GROUP BY company
-ORDER by sum DESC;
+ORDER by laid_off_sum DESC;
 
 SELECT MIN(`date`), MAX(`date`)
 FROM layoffs_staging_2;
 
-SELECT industry, SUM(total_laid_off) AS sum
+SELECT industry, SUM(total_laid_off) AS laid_off_sum
 FROM layoffs_staging_2
 GROUP BY industry
-ORDER BY sum DESC;
+ORDER BY laid_off_sum DESC;
 
-SELECT country, SUM(total_laid_off) AS sum
+SELECT country, SUM(total_laid_off) AS laid_off_sum
 FROM layoffs_staging_2
 GROUP BY country
-ORDER BY sum DESC;
+ORDER BY laid_off_sum DESC;
 
-SELECT YEAR(`date`) AS year_date, SUM(total_laid_off) AS sum
+SELECT YEAR(`date`) AS year_date, SUM(total_laid_off) AS laid_off_sum
 FROM layoffs_staging_2
 GROUP BY year_date
 ORDER BY year_date DESC;
 
-SELECT stage, SUM(total_laid_off) AS sum
+SELECT stage, SUM(total_laid_off) AS laid_off_sum
 FROM layoffs_staging_2
 GROUP BY stage
 ORDER BY stage DESC;
 
-SELECT stage, SUM(total_laid_off) AS sum
+SELECT stage, SUM(total_laid_off) AS laid_off_sum
 FROM layoffs_staging_2
 GROUP BY stage
-ORDER BY sum DESC;
+ORDER BY laid_off_sum DESC;
 
 SELECT company, AVG(percentage_laid_off) AS average
 FROM layoffs_staging_2
@@ -67,25 +67,19 @@ SELECT SUBSTRING(`date`,1,7) AS `MONTH`, SUM(total_laid_off) AS total_off
 FROM layoffs_staging_2
 WHERE SUBSTRING(`date`,1,7) IS NOT NULL
 GROUP BY `MONTH`
-ORDER BY `MONTH` ASC
 )
 SELECT `MONTH`, total_off,
 SUM(total_off) OVER(ORDER BY `MONTH`) AS rolling_total
-FROM Rolling_Total
+FROM Rolling_Total;
 
-SELECT company, SUM(total_laid_off) AS sum
-FROM layoffs_staging_2
-GROUP BY company
-ORDER BY sum DESC;
-
-SELECT company, YEAR(`date`) as year_date, SUM(total_laid_off) as sum
+SELECT company, YEAR(`date`) as year_date, SUM(total_laid_off) as laid_off_sum
 FROM layoffs_staging_2
 GROUP BY company, year_date
-ORDER BY sum ASC;
+ORDER BY laid_off_sum ASC;
 
 WITH Company_Year (company, years, total_laid_off) AS
 (
-SELECT company, YEAR(`date`) as year_date, SUM(total_laid_off) as sum
+SELECT company, YEAR(`date`) as year_date, SUM(total_laid_off) as laid_off_sum
 FROM layoffs_staging_2
 GROUP BY company, year_date
 ), Company_Year_Rank AS
